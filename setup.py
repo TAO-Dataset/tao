abf3317a0abd5ec69d9b1b03c11918506a3624ef
Achal Dave <achalddave@live.com>
2020-06-19 09:26:38 -0400

    Fix pip install (add __init__, call find_packages)

--- setup.py
+++ setup.py
@@ -106,9 +106,9 @@ setup(
     author_email=EMAIL,
     python_requires=REQUIRES_PYTHON,
     url=URL,
-    # packages=find_packages(exclude=["tests", "*.tests", "*.tests.*", "tests.*"]),
+    packages=find_packages(exclude=["tests", "*.tests", "*.tests.*", "tests.*"]),
     # If your package is a single module, use this instead of 'packages':
-    py_modules=['tao'],
+    # py_modules=['tao'],
 
     # entry_points={
     #     'console_scripts': ['mycli=mymodule:cli'],
